1b37352c68231eeaaf82dc24e29bca475e1c188d
DashiellMR <dashiellrich@gmail.com>
2024-01-20 00:33:15 -0500

    added playermovement, inventoryfile, etc

---
+++ steampunk_puzzle/Assets/Inventory.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class InventoryItem{
+    public string itemName;
+    public int itemID;
+}
+public class Inventory{
+    public List<InventoryItem> userItems = new List<InventoryItem>();
+
+    public int addItem(InventoryItem item){
+        userItems.Add(item);
+    }
+
+    public int  removeItem(InventoryItem item){
+        if (userItems.Contains(item)){
+            userItems.Remove(item); 
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    public int combineItems(InventoryItem item1, InventoryItem item2){
+        userItems.Remove(item1);
+        userItems.Remove(item2);
+        //combinedItem = findCombinedItem(item1, item2);
+        //userItems.Add(combinedItem);
+    }
+    public int findCombinedItem(){
+        return 0;
+    }
+}
